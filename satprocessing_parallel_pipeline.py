595fcd197815f56377d52203e881c4c669fc07de
Kimberly Hyde - NOAA <kimberly.hyde@noaa.gov>
2025-09-16 16:59:02 -0400

    Updated environment snippets

--- satprocessing_parallel_pipeline.py
+++ satprocessing_parallel_pipeline.py
@@ -13,11 +13,9 @@ from datetime import datetime
 project_root = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(project_root))
 
-from bootstrap.environment import bootstrap_environment
+from utilities.bootstrap.environment import bootstrap_environment
 env = bootstrap_environment(verbose=False)
 
-from utilities.calc_primprod import run_pp_pipeline
-from utilities.calc_phytosizeclass import run_psc_pipeline
 from utilities.file_utilities import get_prod_files
 from utilities.date_utilities import get_source_file_dates
 
